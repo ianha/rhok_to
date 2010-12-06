bf04675cc18b5f5b0724098851412a94ac9ed08f
Ian Ha <ianha0@gmail.com>
2010-12-06 09:37:07 -0500

    latest change

--- sinatra_test.rb
+++ sinatra_test.rb
@@ -1,7 +1,29 @@
 require 'rubygems'
 require 'sinatra'
-require 'rails'
+require 'nokogiri'
+require 'open-uri'
+require 'pp'
+require 'json'
 
 get '/' do
   "Random hack of kindness!"
 end
+
+get '/example.json' do
+  content_type :json
+  doc = Nokogiri::HTML(open('http://en.wikipedia.org/wiki/List_of_countries_by_population'))
+  country = {}
+  
+  doc.css('table.wikitable tbody tr').each do |node|
+    json[:content] = node.content
+  end
+  
+    # [
+    #   [:country, 'textarea#csi']
+    # ].collect do |name, css|
+    #   json[name] = doc.at_css(css).content
+    # end
+  
+  json.to_json
+end
+  
